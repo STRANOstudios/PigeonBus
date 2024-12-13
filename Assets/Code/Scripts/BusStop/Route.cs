b4ce927ad49045cf2dfab1f96861e3ce1cc57405
STRANOstudios <friand2021@gmail.com>
2024-12-13 21:49:28 +0100

    bugfix

--- Assets/Code/Scripts/BusStop/Route.cs
+++ Assets/Code/Scripts/BusStop/Route.cs
@@ -8,6 +8,9 @@ public class Route : Singleton<Route>
     [SerializeField, Tooltip("The bus livery's color")] private List<Material> _routes = new();
     [SerializeField, Tooltip("The bus stop's color")] private List<Material> _routesBusStop = new();
     [SerializeField] List<GameObject> _busStop = new();
+    [SerializeField, ProgressBar(0, 100, ColorGetter = "GetHealthBarColor"), Tooltip("The chance of a route pack being selected")] private int _difficutly = 50;
+
+    private RoutePack _selectedRoutePack;
 
     private void OnValidate()
     {
@@ -26,6 +29,8 @@ public class Route : Singleton<Route>
 
     private void Awake()
     {
+        _selectedRoutePack = GetRandomRoute();
+
         for (int i = 0; i < _busStop.Count; i++)
         {
             RoutePack routePack = GetRandomRoute();
@@ -42,8 +47,30 @@ public class Route : Singleton<Route>
     /// </summary>
     public RoutePack GetRandomRoute()
     {
-        int i = Random.Range(0, _routes.Count);
-        return new RoutePack(_routes[i], i);
+        int randomValue = Random.Range(0, 100);
+        if (randomValue < _difficutly && _selectedRoutePack != null)
+        {
+            return _selectedRoutePack;
+        }
+        else
+        {
+            int i = Random.Range(0, _routes.Count);
+            return new RoutePack(_routes[i], i);
+        }
+    }
+
+    private Color GetHealthBarColor(float value)
+    {
+        Color orange = new Color(247 / 255f, 91 / 255f, 2 / 255f);
+
+        if (value <= 50f)
+        {
+            return Color.Lerp(Color.red, orange, value / 50f);
+        }
+        else
+        {
+            return Color.Lerp(orange, Color.green, (value - 50f) / 50f);
+        }
     }
 }
 
